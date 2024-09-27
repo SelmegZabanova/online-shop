1b75f1ecb037f605d564c22dd9ed036498bec235
Selmeg <zabanova_selmeg@yandex.ru>
2024-09-27 23:45:15 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$conn = pg_connect("host=postgres_db dbname=mydb user=user password=pass");
+if ($conn) {
+    echo "Connected to PostgreSQL successfully!";
+} else {
+    echo "Connection failed.";
+}
+?>
